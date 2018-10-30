f8e6f9c11176e641a2a00071cba11e14282d46b2
Gwynbleidd96 <alessandro.pozzi72@gmail.com>
2018-10-30 17:13:26 +0100

    Création de la page d'accueil

---
+++ app/src/main/java/com/henallux/dondesang/DonnerSang.java
@@ -0,0 +1,13 @@
+package com.henallux.dondesang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonnerSang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donner_sang);
+    }
+}
